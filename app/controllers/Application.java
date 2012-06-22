6541f4306e20430b94c677a5b5919f4281ed8bf1
Yinan Yang <yinan.yang@optusnet.com.au>
2012-06-22 17:13:25 +1000

    set up capture

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,11 @@ import play.mvc.*;
 import java.util.*;
 import models.*;
 import play.data.validation.*;
+import play.libs.Codec;
+import play.libs.Images;
+import play.cache.Cache;
+
+
 
 public class Application extends Controller {
 	//render front page
@@ -23,18 +28,38 @@ public class Application extends Controller {
     
     public static void show(Long id){
     		Post post = Post.findById(id);
-    		render(post);
+    		//this is to generate a unit id for captcha
+    		String randomID = Codec.UUID();
+    		render(post,randomID);
     }
     
-    public static void postComment(@Required Long postId, @Required String author, @Required String content){
+    public static void postComment(Long postId, 
+    							   @Required (message = "Author is required")String author,
+    							   @Required (message = "Please fill in some content")String content,
+    							   @Required (message = "Please enter the code")String code,
+    							   String randomID
+    							   ){
 		 Post post = Post.findById(postId);
 		 
+		 validation.equals(code,Cache.get(randomID)).message("Ivalide code, please type again");
+		 
 		 if(validation.hasErrors()){
-			 render("Application/show.html",post);
+			 render("Application/show.html",post,randomID);
 		 }
 		 
 		 post.addComment(author, content);
+		 
+		 flash.success("Thanks for posting %s", author);
+		 Cache.delete(randomID);
 		 show(postId);
 	}
+    
+    public static void captcha(String id){
+    	Images.Captcha captcha = Images.captcha(200,50);
+    	captcha.setSquigglesBackground();
+    	String code = captcha.getText();
+    	Cache.set(id, code, "10mn");
+    	renderBinary(captcha);
+    }
 
 }
